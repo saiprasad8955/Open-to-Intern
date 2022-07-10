df637d22d79b522b1d793c86e633d81208ae7e69
saiprasad <rsaiprasad4@gmail.com>
2022-07-10 21:00:47 +0530

    ENV FILE ADDED

--- src/index.js
+++ src/index.js
@@ -1,17 +1,14 @@
+require('dotenv').config()
 const express = require("express");
 const bodyParser = require("body-parser");
-const route = require("./route/route.js"); 
+const route = require("./route/route.js");
 const { default: mongoose } = require("mongoose");
 const app = express();
 
 app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({ extended: true }));
 
-mongoose.connect("mongodb+srv://Sai0047:rXxgqYKPqwnhcXX7@cluster0.qptsw.mongodb.net/open_to_intern_project_2",
-    {
-      useNewUrlParser: true,
-    }
-  )
+mongoose.connect(process.env.MONGO_URI, { useNewUrlParser: true, })
   .then(() => console.log("MongoDb is connected"))
   .catch((err) => console.log(err));
 
